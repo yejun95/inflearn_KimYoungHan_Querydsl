00c948b79ceb99dfdeb4d4e9a64eb492923c135c
xodlf100 <xodlf100@gmail.com>
2024-12-21 17:02:13 +0900

    feat 집합 + group by

--- querydsl/src/test/java/study/querydsl/QueryDslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -9,18 +10,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QTeam.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 public class QueryDslBasicTest {
 
     @Autowired
@@ -47,7 +52,9 @@ public class QueryDslBasicTest {
         em.persist(member4);
     }
 
-    //JPQL을 이용해 member1을 찾기
+    /**
+     * JPQL을 이용해 member1을 찾기
+     */
     @Test
     public void startJPQL() {
         Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
@@ -57,7 +64,9 @@ public class QueryDslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
-    //querydsl 이용해 member1을 찾기
+    /**
+     * querydsl 이용해 member1을 찾기
+     */
     @Test
     public void startQuerydsl() {
         Member findMember = queryFactory
@@ -69,7 +78,9 @@ public class QueryDslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
-    //검색 조건
+    /**
+     * 검색 조건
+     */
     @Test
     public void search() {
         Member findMember = queryFactory
@@ -94,7 +105,9 @@ public class QueryDslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
-    //결과 조회
+    /**
+     * 결과 조회
+     */
     @Test
     public void resultFetch() {
         // 리스트 조회 : fetch()
@@ -154,7 +167,9 @@ public class QueryDslBasicTest {
         assertThat(memberNull.getUsername()).isEqualTo(null);
     }
 
-    //페이징
+    /**
+     * 페이징
+     */
     @Test
     public void paging1() {
         List<Member> result = queryFactory
@@ -167,7 +182,7 @@ public class QueryDslBasicTest {
         assertThat(result.size()).isEqualTo(2);
     }
 
-    //페이징 : fetchResults를 이용한 데이터 추출
+    //fetchResults를 이용한 데이터 추출
     @Test
     public void paging2() {
         QueryResults<Member> queryResults = queryFactory
@@ -181,5 +196,50 @@ public class QueryDslBasicTest {
         assertThat(queryResults.getLimit()).isEqualTo(2);
         assertThat(queryResults.getOffset()).isEqualTo(1);
         assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    /**
+     * 집합
+     */
+    @Test
+    public void aggregation() {
+        List<Tuple> result = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    // group by
+    @Test
+    void group() {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+//                .having() having 조건도 추가 가능
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
 }

b1e5308e89057b8504ebf2c822e678eabe94a888
xodlf100 <xodlf100@gmail.com>
2024-12-22 21:12:21 +0900

    feat 순수 jpa  vs querydsl 간단 로직 비교

--- querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,10 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -10,4 +13,10 @@ public class QuerydslApplication {
         SpringApplication.run(QuerydslApplication.class, args);
     }
 
+
+    /*스프링 빈에 QueryFactory 등록법
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }*/
 }
